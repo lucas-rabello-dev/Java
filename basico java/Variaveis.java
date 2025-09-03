b6ccffffb450aec034b12399d547df9631b4fde3
Lucas Rabello <lucasrabello2020@gmail.com>
2025-09-03 19:09:51 -0300

    Aula 1 java

---
+++ basico java/Variaveis.java
@@ -0,0 +1,10 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        // <tipo> <nome> = <valor>;
+        double x = 3.14159;
+        System.out.println(x);
+        System.out.printf("%.2f \n", x); // printa , pois depende do idioma do seu computador
+        char word = 'A';
+        System.out.printf("%c \n", word);
+    }
+}
