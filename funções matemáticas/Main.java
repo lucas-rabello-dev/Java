d214e560bbae74a55613948972671bc37af3beb3
Lucas Rabello <lucasrabello2020@gmail.com>
2025-09-22 18:06:28 -0300

    Update

--- funções matemáticas/Main.java
+++ funções matemáticas/Main.java
@@ -1,6 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
-        
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+        System.out.printf("Raiz de %.2f = %.2f \n", x, A);
+        System.out.printf("Raiz de %.2f = %.2f \n", y, B);
+        System.out.printf("Raiz de 25 = %.2f \n", C);
+
+        double teste1 = Math.pow(2, 3); // potência 2^3
+        double teste2 = Math.abs(z); // valor absoluto (se for negativo fica positivo)
+        System.out.println(teste1 + " | " + teste2); 
+
+
     }
 }
